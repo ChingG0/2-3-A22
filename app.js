54cb102e90274635e73cf22dcc05c679e9480b4e
ChingG0 <www.karta2428620@gmail.com>
2022-07-08 15:36:54 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express()
+const port = 3000;
+const exphbs = require('express-handlebars')
+app.engine('handlebars', exphbs.engine({ defaultLayout: "main"}))
+app.set("view engine", "handlebars")
+
+app.get("/",(req,res)=>{
+    res.render("index")
+})
+
+app.get("/:page",(req,res)=>{
+    res.render(`${req.params.page}`)
+})
+
+app.listen(port, ()=>{
+    console.log(`Express is listening on localhost: ${port}`);
+})
